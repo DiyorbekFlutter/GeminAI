52b28e1cef4918cb7ae12a75dfabec07f7af909a
Boburbek6010 <boburbekkhusanboev@gmail.com>
2024-02-28 21:58:24 +0500

    Initial commit

---
+++ bin/c_group_chat_with_ai.dart
@@ -0,0 +1,5 @@
+import 'package:c_group_chat_with_ai/c_group_chat_with_ai.dart' as c_group_chat_with_ai;
+
+void main(List<String> arguments) {
+  print('Hello world: ${c_group_chat_with_ai.calculate()}!');
+}
