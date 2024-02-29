926c46504689eceea46c40dc3e175f6974114105
ahmad <tdax962@gmail.com>
2024-02-29 07:53:16 +0500

    Shohahtam 4 commit menu qo'shildi

---
+++ lib/menu.dart
@@ -0,0 +1,21 @@
+abstract class Menu{
+  Menu menu;
+
+  Menu({
+    required this.menu
+});
+}
+class  ProfileMenu extends Menu{
+
+
+  ProfileMenu({
+    required super.menu
+});
+}
+
+class Settings extends Menu{
+
+  Settings({
+    required super.menu
+});
+}
